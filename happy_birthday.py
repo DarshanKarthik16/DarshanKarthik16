3fff521617ca5ed2b6b29ba61b85dcc788a98e32
DarshanKarthik16 <163162330+DarshanKarthik16@users.noreply.github.com>
2024-09-04 14:52:35 +0400

    Add files via upload

---
+++ happy_birthday.py
@@ -0,0 +1,26 @@
+
+def birthday_message():
+
+    print("what is your name")
+    name = input()
+    print("what is your birth month")
+    month = input()
+    print("what is the date")
+    date = input()
+    print("what is the year")
+    year = input()
+
+    print(name + ", your birthday is on " + month + " " + date + ", " + year)
+
+
+
+print(birthday_message())
+
+
+
+
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,9 @@
+print("write your name: ")
+name = input()
+print("My name is " + name)
+
+_name = "ali"
+print(_name)
+
+
+
